e4d4c0e44669815eb9985cd25116345f22ac942f
Brandon Giron <thelordgiron@gmail.com>
2018-12-26 14:47:30 -0800

    Created seeds file for sample data

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(version: 2018_12_26_175324) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
